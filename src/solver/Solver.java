0f75925c1e0b32c2a04cf2ccbbbcf4629fe88648
fsb4000 <fsb4000@yandex.ru>
2018-12-18 23:49:49 +0700

    add multiply row to constant manipulation

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -3,18 +3,44 @@ package solver;
 import java.util.Scanner;
 
 public class Solver {
-    private double matrix[][];
+    private double[][] matrix;
+    private double[] solution;
     public Solver(Scanner sc) {
         final int n = sc.nextInt();
         matrix = new double[n][n+1];
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
+            for (int j = 0; j < n+1; ++j) {
                 matrix[i][j] = sc.nextDouble();
             }
         }
+        solution = new double[n];
     }
 
     public int getSize() {
         return matrix.length;
+    }
+
+    private void multiplyRow(int row, double k) {
+        final int n = matrix[row].length;
+        for (int i = 0; i < n; ++i) {
+            matrix[row][i] *= k;
+        }
+    }
+
+    public void solve() {
+        final int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            if (matrix[i][i] != 1.0) {
+                final double k = 1.0 / matrix[i][i];
+                multiplyRow(i, k);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            solution[i] = matrix[i][n];
+        }
+    }
+
+    public double[] getSolution() {
+        return solution;
     }
 }
--- test/SolverTest.java
+++ test/SolverTest.java
@@ -11,4 +11,12 @@ public class SolverTest {
         final Solver p = new Solver(sc);
         Assert.assertEquals(1, p.getSize());
     }
+
+    @Test
+    public void solve() {
+        final Scanner sc = new Scanner("1\n2 4");
+        final Solver p = new Solver(sc);
+        p.solve();
+        Assert.assertArrayEquals(new double[]{2.0}, p.getSolution(), 0.0000001);
+    }
 }
