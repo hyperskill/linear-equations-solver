7622c6e2f86ec86bdd3e901fb16b9c4977f1a0c9
User Fsb4000 <fsb4000@yandex.ru>
2019-01-12 04:54:57 +0700

    C: add FreeBSD support

--- C/drd.sh
+++ C/drd.sh
@@ -9,4 +9,4 @@ case $OS in
   *) ;;
 esac
 
-make clean && VALGRIND=1 make && valgrind --log-file=drd.txt --read-var-info=yes --tool=drd bin/test_cpp
+make clean && VALGRIND=1 make && valgrind --log-file=drd.txt --read-var-info=yes --tool=drd bin/test_c
