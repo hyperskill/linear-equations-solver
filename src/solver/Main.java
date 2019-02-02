0775df308e3bb4c3298e1ec2e833f5593de45df0
Kirill <thegaldren@gmail.com>
2019-02-02 13:04:29 +0300

    Чтение файла

--- src/solver/Main.java
+++ src/solver/Main.java
@@ -1,7 +1,24 @@
 package solver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        String in = args[1];
+        String out = args[3];
+
+        File file = new File(in);
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine())
+                System.out.println(scanner.nextLine());
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
