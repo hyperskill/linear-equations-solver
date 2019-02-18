65503cc92a4ba659eca0afcd89081db3e3c2b74b
Grokholskaya <grokholskie@gmail.com>
2019-02-18 15:18:04 +0300

    adding new files

---
+++ src/stage1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class stage1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        //a * x = b
+        System.out.println(b / a);
+    }
+}
